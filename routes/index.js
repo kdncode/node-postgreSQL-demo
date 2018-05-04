57ac61b207271d72bb6f668a7a6ec94a2d1ab2bc
kdncode <kdncode@gmail.com>
2018-05-04 02:30:35 -0500

    Added CREATE

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const { Pool, Client } = require('pg')
+const { Pool, Client } = require('pg') // Programmatic (https://node-postgres.com/features/connecting)
 
+// Connect db
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
@@ -10,14 +11,29 @@ const pool = new Pool({
   port: 5432,
 })
 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  pool.query('SELECT * FROM contact', (err, res) => {
-  console.log(err, res)
-  pool.end()
-})
+    pool.query('SELECT * FROM contact', (err, res) => {
+    console.log(err, res)
+    pool.end()
+  })
   res.render('index', { title: 'Express' });
 });
+
+// CREATE db - GET
+router.get('/new', function(req, res, next) {
+  res.render('new', { title: 'Add New Data' });
+});
+
+// CREATE db - POST
+router.post('/new', function(req, res, next) {
+  // Get data
+  var name = req.body.name, age = req.body.age;
+  pool.query('INSERT INTO contact (name, age) VALUES ($1, $2)', [name, age], (err, res) => {
+    pool.end()
+  });
+  res.render('new', { title: 'Add New Data' });
+});
+
 
 module.exports = router;
